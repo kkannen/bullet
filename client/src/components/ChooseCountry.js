4fb3ed5e0dd98214d9455751446649edee650e0d
kkannen <kristamkannen@gmail.com>
2018-11-05 20:27:44 -0600

    moved redux and component folders into src

---
+++ client/src/components/ChooseCountry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ChooseCountry extends React.Component {
+    render () {
+        return (
+            <h1>POOOOOOOOOOOO</h1>
+        )
+    }
+}
+
+export default ChooseCountry;
